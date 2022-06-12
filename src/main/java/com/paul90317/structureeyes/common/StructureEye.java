e804b4bd2d6d7ed47fa7c3d7cd63559f82bc0d64
paul <paul2309825@gmail.com>
2022-06-12 17:30:01 +0800

    4eyes

--- src/main/java/com/paul90317/structureeyes/common/StructureEye.java
+++ src/main/java/com/paul90317/structureeyes/common/StructureEye.java
@@ -1,7 +1,12 @@
 package com.paul90317.structureeyes.common;
 
+import com.mojang.datafixers.util.Pair;
 import com.paul90317.structureeyes.MainMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -15,79 +20,58 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+
 public class StructureEye extends Item {
-    final private TagKey<ConfiguredStructureFeature<?,?>> structure;
-    public StructureEye(TagKey<ConfiguredStructureFeature<?,?>> _structure) {
+    final private TagKey<ConfiguredStructureFeature<?,?>> structureTagKey;
+    final private ResourceKey<ConfiguredStructureFeature<?,?>> structureResourceKey;
+    final boolean isHolder;
+    public StructureEye(TagKey<ConfiguredStructureFeature<?,?>> structure) {
         super(new Properties().stacksTo(64).tab(MainMod.MOD_TAB));
-        structure=_structure;
+        isHolder=false;
+        structureTagKey=structure;
+        structureResourceKey=null;
     }
-
-    /*public InteractionResult useOn(UseOnContext p_41182_) {
-        Level level = p_41182_.getLevel();
-        BlockPos blockpos = p_41182_.getClickedPos();
-        BlockState blockstate = level.getBlockState(blockpos);
-        if (blockstate.is(Blocks.END_PORTAL_FRAME) && !blockstate.getValue(EndPortalFrameBlock.HAS_EYE)) {
-            if (level.isClientSide) {
-                return InteractionResult.SUCCESS;
-            } else {
-                BlockState blockstate1 = blockstate.setValue(EndPortalFrameBlock.HAS_EYE, Boolean.valueOf(true));
-                Block.pushEntitiesUp(blockstate, blockstate1, level, blockpos);
-                level.setBlock(blockpos, blockstate1, 2);
-                level.updateNeighbourForOutputSignal(blockpos, Blocks.END_PORTAL_FRAME);
-                p_41182_.getItemInHand().shrink(1);
-                level.levelEvent(1503, blockpos, 0);
-                BlockPattern.BlockPatternMatch blockpattern$blockpatternmatch = EndPortalFrameBlock.getOrCreatePortalShape().find(level, blockpos);
-                if (blockpattern$blockpatternmatch != null) {
-                    BlockPos blockpos1 = blockpattern$blockpatternmatch.getFrontTopLeft().offset(-3, 0, -3);
-
-                    for(int i = 0; i < 3; ++i) {
-                        for(int j = 0; j < 3; ++j) {
-                            level.setBlock(blockpos1.offset(i, 0, j), Blocks.END_PORTAL.defaultBlockState(), 2);
-                        }
-                    }
-
-                    level.globalLevelEvent(1038, blockpos1.offset(1, 0, 1), 0);
-                }
-
-                return InteractionResult.CONSUME;
-            }
+    public StructureEye(ResourceKey<ConfiguredStructureFeature<?,?>> structure) {
+        super(new Properties().stacksTo(64).tab(MainMod.MOD_TAB));
+        isHolder=true;
+        structureTagKey=null;
+        structureResourceKey=structure;
+    }
+    private BlockPos locateStructure(ServerLevel serverLevel, BlockPos blockPos) {
+        Registry<ConfiguredStructureFeature<?,?>> registry = serverLevel.registryAccess().registryOrThrow(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY);
+        Holder<ConfiguredStructureFeature<?,?>> structureHolder=registry.getHolder(structureResourceKey).get();
+        HolderSet<ConfiguredStructureFeature<?,?>> structureHolderSet=HolderSet.direct(structureHolder);
+        Pair<BlockPos, Holder<ConfiguredStructureFeature<?,?>>> pair = serverLevel.getChunkSource().getGenerator().findNearestMapFeature(serverLevel, structureHolderSet, blockPos, 100, false);
+        if (pair == null) {
+            return null;
         } else {
-            return InteractionResult.PASS;
+            return pair.getFirst();
         }
-    }*/
-
+    }
     @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand handItem) {
-        ItemStack itemstack = player.getItemInHand(handItem);
-        /*HitResult hitresult = getPlayerPOVHitResult(world, player, ClipContext.Fluid.NONE);
-        if (hitresult.getType() == HitResult.Type.BLOCK && world.getBlockState(((BlockHitResult)hitresult).getBlockPos()).is(Blocks.END_PORTAL_FRAME)) {
-            return InteractionResultHolder.pass(itemstack);
-        } else {*/
-            player.startUsingItem(handItem);
-            if (world instanceof ServerLevel serverlevel) {
-                BlockPos blockpos = serverlevel.findNearestMapFeature(structure, player.blockPosition(), 100, false);
-                if (blockpos != null) {
-                    EyeOfEnder eyeofender = new EyeOfEnder(world, player.getX(), player.getY(0.5D), player.getZ());
-                    eyeofender.setItem(itemstack);
-                    eyeofender.signalTo(blockpos);
-                    world.addFreshEntity(eyeofender);
-                    /*if (player instanceof ServerPlayer) {
-                        CriteriaTriggers.USED_ENDER_EYE.trigger((ServerPlayer)player, blockpos);
-                    }*/
-
-                    world.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDER_EYE_LAUNCH, SoundSource.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-                    world.levelEvent((Player)null, 1003, player.blockPosition(), 0);
-                    if (!player.getAbilities().instabuild) {
-                        itemstack.shrink(1);
-                    }
-
-                    player.awardStat(Stats.ITEM_USED.get(this));
-                    player.swing(handItem, true);
-                    return InteractionResultHolder.success(itemstack);
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        player.startUsingItem(hand);
+        if (world instanceof ServerLevel serverlevel){
+            BlockPos blockpos = isHolder? locateStructure(serverlevel,new BlockPos(player.getPosition(0.5f)))
+                    :serverlevel
+                    .findNearestMapFeature(structureTagKey, player.blockPosition(), 100, false);
+            if (blockpos != null) {
+                EyeOfEnder eyeofender = new EyeOfEnder(world, player.getX(), player.getY(0.5D), player.getZ());
+                eyeofender.setItem(itemstack);
+                eyeofender.signalTo(blockpos);
+                world.addFreshEntity(eyeofender);
+                world.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDER_EYE_LAUNCH, SoundSource.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+                world.levelEvent((Player)null, 1003, player.blockPosition(), 0);
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
                 }
-            }
 
-            return InteractionResultHolder.consume(itemstack);
-        //}
+                player.awardStat(Stats.ITEM_USED.get(this));
+                player.swing(hand, true);
+                return InteractionResultHolder.success(itemstack);
+            }
+        }
+        return InteractionResultHolder.consume(itemstack);
     }
 }
--- src/main/java/com/paul90317/structureeyes/core/ModItems.java
+++ src/main/java/com/paul90317/structureeyes/core/ModItems.java
@@ -4,21 +4,23 @@ import com.paul90317.structureeyes.MainMod;
 import com.paul90317.structureeyes.common.StructureEye;
 import net.minecraft.tags.ConfiguredStructureTags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.function.Supplier;
-
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MainMod.MOD_ID);
-    public static final RegistryObject<Item> VillageEye = ITEMS.register("village_eye",
-            () -> new StructureEye(ConfiguredStructureTags.VILLAGE));
-    /*public static final RegistryObject<Item> EndCityEye = ITEMS.register("end_city_eye",
-            () -> new StructureEye(ConfiguredStructureTags.));
-    public static final RegistryObject<Item> FortressEye = ITEMS.register("fortress_eye",
-            () -> new net.hellopaul.structureeyes.item.StructureEye(Structure.FORTRESS));
-    public static final RegistryObject<Item> BastionRemnantEye = ITEMS.register("bastion_remnant_eye",
-            () -> new net.hellopaul.structureeyes.item.StructureEye(Structure.BASTION_REMNANT));*/
+    public static final RegistryObject<Item> VillageEye,EndCityEye,FortressEye,BastionRemnantEye;
+    static {
+        VillageEye = ITEMS.register("village_eye",
+                () -> new StructureEye(ConfiguredStructureTags.VILLAGE));
+        EndCityEye = ITEMS.register("end_city_eye",
+                () -> new StructureEye(BuiltinStructures.END_CITY));
+        FortressEye = ITEMS.register("fortress_eye",
+                () -> new StructureEye(BuiltinStructures.FORTRESS));
+        BastionRemnantEye = ITEMS.register("bastion_remnant_eye",
+                () -> new StructureEye(BuiltinStructures.BASTION_REMNANT));
+    }
 }
